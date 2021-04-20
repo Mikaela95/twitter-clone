72a331499cba94b77518611ef0f0e24057e66500
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2021-04-20 18:51:46 +1000

    incorporating TypeORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,21 @@
-/* import express from "express";
-const app = express();
-const port = 8080; // default port to listen
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+createConnection().then(async connection => {
 
-// start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} ); */
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
 
-console.log("hello world!!!")
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
+
+    console.log("Here you can setup and run express/koa/any other framework.");
+
+}).catch(error => console.log(error));
