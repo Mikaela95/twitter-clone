bf0ec26d872fe5fd568daac45bace5b42b8486fe
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2021-04-28 07:52:33 +1000

    moved apollo client setup to top level file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,17 @@
 import React from "react";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
-import "./App.css";
-import { Users } from "./components/Users"
-
+import { Users } from "./components/Users";
 
 // Instance of the apollo client
-const client = new ApolloClient({
+/* const client = new ApolloClient({
   cache: new InMemoryCache(),
   uri: "http://localhost:4000/graphql"
-});
+}); */
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <div>
-        <Users/>
-      </div>
-    </ApolloProvider>
+    <div>
+      <Users />
+    </div>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 import reportWebVitals from './reportWebVitals';
 
+// Instance of the apollo client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "http://localhost:4000/graphql"
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+    </ApolloProvider>,
   document.getElementById('root')
 );
 
