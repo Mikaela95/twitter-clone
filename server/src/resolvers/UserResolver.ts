828ba05dadc661c5b998168de5a8d95e10cb24da
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2021-04-27 17:33:28 +1000

    jsonwebtoken and cookies

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,14 +1,21 @@
-import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import { User } from "../entity/User";
 import bcrypt from "bcrypt";
-import { context } from "src/types.ts/context";
+import { sign } from "jsonwebtoken";
+import { context } from "../types.ts/context";
 
 // @Resolver is a decorator - the class you define after this will behave as a controller
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string
+}
 
 // Object that is passed in
 @InputType()
 class UsernamePasswordInput {
-  
+
   @Field()
   username: string
 
@@ -24,10 +31,29 @@ export class UserResolver {
     return User.find()
   }
 
-  // Ability to create a user
+  // Create a user
   @Mutation(() => User)
   async createUser(@Arg("options") options: UsernamePasswordInput) {
     const hashedPassword = bcrypt.hashSync(options.password, bcrypt.genSaltSync(10));
     return User.create({ username: options.username, password: hashedPassword }).save();
   }
+
+  @Mutation(() => LoginResponse)
+  async loginUser(@Arg("options") options: UsernamePasswordInput, @Ctx() { res }: context) {
+    const user = await User.findOne({ where: { username: options.username } });
+
+    // Check to see if user is created
+    if (!user) {
+      throw new Error("Could not find user")
+    }
+
+    // To-do: check that the password matches
+
+    res.cookie('rick', sign({ userId: user.id }, 'oiueowjlscfewg', { expiresIn: "7d" }), { httpOnly: true })
+
+    return {
+      accessToken: sign({ userId: user.id }, 'mtnvouahroiwjqkl', { expiresIn: "10h" })
+    };
+  }
+
 }
