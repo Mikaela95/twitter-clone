e70628687f7462530ede6cd7f595fdc2756799cb
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2021-05-02 09:12:55 +1000

    place dummy avatar picture

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -6,11 +6,13 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useCurrentUserQuery } from "../generated/graphql";
+import Image from "react-bootstrap/Image";
+import profileImage from "../images/Mikaela_Verhoosel.jpg";
+import "./Home.css";
 
 export const Home = () => {
-  const { data } = useCurrentUserQuery();
   // Need to get user that has just logged in - can use the authentication token
-
+  const { data } = useCurrentUserQuery();
   return (
     <Container style={{ marginTop: "50px" }}>
       <Row>
@@ -19,8 +21,12 @@ export const Home = () => {
         </Col>
         <Col xs={6}>
           {data && data.currentUser ? (
-            <p>the current user is: {data.currentUser.username}</p>
-          ) : <p>The user is not currently logged in</p>}
+            <div>
+              <p>the current user is: {data.currentUser.username}</p>{" "}
+            </div>
+          ) : (
+            <p>The user is not currently logged in</p>
+          )}
           <Post />
         </Col>
         <Col>
--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -1,20 +1,36 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import Form from 'react-bootstrap/Form'
+import Form from "react-bootstrap/Form";
+import { useCurrentUserQuery } from "../generated/graphql";
+import Image from "react-bootstrap/Image";
+import profileImage from "../images/Mikaela_Verhoosel.jpg";
 
 // Able to send tweet and appear in feed - populate with dummy data that the user can comment, retweet and like
 
 export const Post = () => {
+  const { data } = useCurrentUserQuery();
+
   return (
     <Card>
       <Card.Header as="h5">Home</Card.Header>
       <Card.Body>
-        <Card.Title>User image to come</Card.Title>
+        <Card.Title>
+          <Image
+            src={profileImage}
+            roundedCircle
+            style={{ width: "50px", height: "50px" }}
+          />
+        </Card.Title>
         <Card.Text>
-          <Form.Control id="tweet-input" type="password" placeholder="What's happening?" />
+          <Form.Control
+            id="tweet-input"
+            placeholder="What's happening?"
+          />
         </Card.Text>
-        <Button variant="primary" type="submit">Tweet</Button>
+        <Button variant="primary" type="submit">
+          Tweet
+        </Button>
       </Card.Body>
     </Card>
   );
