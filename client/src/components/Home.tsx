e31ffacc5fd328c1637f46440484acff5330a28a
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2021-05-03 20:08:05 +1000

    minor styling to fetched tweets and clean up of unused code

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -5,15 +5,13 @@ import { Post } from "./Post";
 import { Loading } from "./Loading";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
+import CardColumns from "react-bootstrap/CardColumns";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useCurrentUserQuery, useTweetQuery } from "../generated/graphql";
 import Image from "react-bootstrap/Image";
 import profileImage from "../images/Mikaela_Verhoosel.jpg";
 import "./Home.css";
-import { useEffect } from "react";
-import { useState } from "react";
 
 export const Home = () => {
   // Need to get user that has just logged in - can use the authentication token
@@ -39,23 +37,19 @@ export const Home = () => {
             <p>The user is not currently logged in</p>
           )}
           <Post />
-          <CardGroup>
-            <Row md={4} style={{ justifyContent: "space-evenly" }}>
-              {dataT.tweets.map((tweet: any) => (
-                <Card className="categoryCard" key={tweet._id}>
-                  <Card.Body style={{ color: "black" }}>
-                    <Card.Title>Some title</Card.Title>
-                    <Card.Text>{tweet.content}</Card.Text>
-                  </Card.Body>
-                  <Card.Footer>
-                    <small className="text-muted">
-                      Last updated 3 mins ago
-                    </small>
-                  </Card.Footer>
-                </Card>
-              ))}
-            </Row>
-          </CardGroup>
+          <CardColumns style={{marginTop: "1rem"}}>
+            {dataT.tweets.map((tweet: any) => (
+              <Card className="categoryCard" key={tweet._id}>
+                <Card.Body style={{ color: "black" }}>
+                  <Card.Title>Some title</Card.Title>
+                  <Card.Text>{tweet.content}</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+              </Card>
+            ))}
+          </CardColumns>
         </Col>
         <Col>
           <Search />
